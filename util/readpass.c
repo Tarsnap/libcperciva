63fb14af90ea13587767f479d369bc52c23a5311
Colin Percival <cperciva@tarsnap.com>
2013-05-31 23:30:11 +0000

    On second thought, use #define to avoid namespace collisions, as we do in other routines here...

--- util/readpass.c
+++ util/readpass.c
@@ -35,18 +35,17 @@ handle(int sig)
 }
 
 /**
- * libcperciva_readpass(passwd, prompt, confirmprompt, devtty)
+ * readpass(passwd, prompt, confirmprompt, devtty)
  * If ${devtty} is non-zero, read a password from /dev/tty if possible; if
  * not, read from stdin.  If reading from a tty (either /dev/tty or stdin),
  * disable echo and prompt the user by printing ${prompt} to stderr.  If
  * ${confirmprompt} is non-NULL, read a second password (prompting if a
  * terminal is being used) and repeat until the user enters the same password
  * twice.  Return the password as a malloced NUL-terminated string via
- * ${passwd}.  The obscure name is to avoid namespace collisions due to the
- * getpass / readpass / readpassphrase / etc. functions in various libraries.
+ * ${passwd}.
  */
 int
-libcperciva_readpass(char ** passwd, const char * prompt,
+readpass(char ** passwd, const char * prompt,
     const char * confirmprompt, int devtty)
 {
 	FILE * readfrom;
