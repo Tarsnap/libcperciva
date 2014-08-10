3d52c6a9e934fe245440e21fcad6c446035083b2
cperciva <cperciva@94510cbd-7082-6b3d-ae8c-869d587649c8>
2014-08-10 03:46:10 +0000

    Add some explanatory comments.

--- trunk/cpusupport/cpusupport.h
+++ trunk/cpusupport/cpusupport.h
@@ -1,10 +1,23 @@
 #ifndef _CPUSUPPORT_H_
 #define _CPUSUPPORT_H_
 
+/*
+ * To enable support for non-portable CPU features at compile time, one or
+ * more CPUSUPPORT_ARCH_FEATURE macros should be defined.  This can be done
+ * directly on the compiler command line; or a file can be created with the
+ * necessary #define lines and then -D CPUSUPPORT_CONFIG_FILE=cpuconfig.h
+ * (or similar) can be provided to include that file here.
+ */
 #ifdef CPUSUPPORT_CONFIG_FILE
 #include CPUSUPPORT_CONFIG_FILE
 #endif
 
+/*
+ * The CPUSUPPORT_FEATURE macro declares the necessary variables and
+ * functions for detecting CPU feature support at run time.  The function
+ * defined in the macro acts to cache the result of the ..._detect function
+ * using the ..._present and ..._init variables.
+ */
 #define CPUSUPPORT_FEATURE(arch, feature)				\
 	extern int cpusupport_ ## arch ## _ ## feature ## _present;	\
 	extern int cpusupport_ ## arch ## _ ## feature ## _init;	\
@@ -25,6 +38,12 @@
 	}								\
 	struct cpusupport_ ## arch ## _ ## feature ## _dummy
 
+/*
+ * Any features listed here must have associated C files compiled and linked
+ * in, since the macro references symbols which must be defined.  Projects
+ * which do not need to detect certain CPU features may wish to remove lines
+ * from this list so that the associated C files can be omitted.
+ */
 CPUSUPPORT_FEATURE(x86, aesni);
 
 #endif /* !_CPUSUPPORT_H_ */
