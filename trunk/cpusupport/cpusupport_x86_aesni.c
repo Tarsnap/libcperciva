220635eb3ae5ec89ccd79079c8b97e09e6a571c7
cperciva <cperciva@94510cbd-7082-6b3d-ae8c-869d587649c8>
2014-08-10 07:52:44 +0000

    Check if the CPU supports level-1 CPUID.  The intrinsic provided by gcc does this for us automatically; the intrinsic provided by clang does not.

--- trunk/cpusupport/cpusupport_x86_aesni.c
+++ trunk/cpusupport/cpusupport_x86_aesni.c
@@ -11,6 +11,13 @@ CPUSUPPORT_FEATURE_DECL(x86, aesni)
 #ifdef CPUSUPPORT_X86_CPUID
 	unsigned int eax, ebx, ecx, edx;
 
+	/* Check if CPUID supports the level we need. */
+	if (!__get_cpuid(0, &eax, &ebx, &ecx, &edx))
+		goto unsupported;
+	if (eax < 1)
+		goto unsupported;
+
+	/* Ask about CPU features. */
 	if (!__get_cpuid(1, &eax, &ebx, &ecx, &edx))
 		goto unsupported;
 
