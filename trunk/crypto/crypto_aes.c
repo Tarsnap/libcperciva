5c8b7280e497a775517d515a394a132300c78546
cperciva <cperciva@94510cbd-7082-6b3d-ae8c-869d587649c8>
2014-08-10 17:43:00 +0000

    Style conformance: Blank line separating (lack of) variables from code in aes_encrypt_block; spaces around '*' in function parameters.

--- trunk/crypto/crypto_aes.c
+++ trunk/crypto/crypto_aes.c
@@ -9,8 +9,9 @@
 void aes_encrypt_block_aesni(const uint8_t *, uint8_t *, const AES_KEY *);
 
 void
-aes_encrypt_block(const uint8_t *in, uint8_t *out, const AES_KEY *key)
+aes_encrypt_block(const uint8_t * in, uint8_t * out, const AES_KEY * key)
 {
+
 #ifdef CPUSUPPORT_X86_AESNI
 	if (cpusupport_x86_aesni())
 		return aes_encrypt_block_aesni(in, out, key);
