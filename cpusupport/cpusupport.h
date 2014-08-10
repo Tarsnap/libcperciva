a58661f20ac44a8a76556548ff46f37bced48a33
Colin Percival <cperciva@tarsnap.com>
2014-08-10 03:33:03 +0000

    Style compliance: * Use tabs to align (most of) the line-continuation backslashes in the   macro definition. * Add an empty line between (lack of) variables in the cpusupport_x_y   function and the code. * Add a dummy structure definition to the macro so that the macro can   be invoked with a terminating semicolon.

--- cpusupport/cpusupport.h
+++ cpusupport/cpusupport.h
@@ -5,14 +5,15 @@
 #include CPUSUPPORT_CONFIG_FILE
 #endif
 
-#define CPUSUPPORT_FEATURE(arch, feature)	\
+#define CPUSUPPORT_FEATURE(arch, feature)				\
 	extern int cpusupport_ ## arch ## _ ## feature ## _present;	\
 	extern int cpusupport_ ## arch ## _ ## feature ## _init;	\
 	int cpusupport_ ## arch ## _ ## feature ## _detect(void);	\
-							\
-	static inline int				\
-	cpusupport_ ## arch ## _ ## feature(void)		\
-	{						\
+									\
+	static inline int						\
+	cpusupport_ ## arch ## _ ## feature(void)			\
+	{								\
+									\
 		if (cpusupport_ ## arch ## _ ## feature ## _present)	\
 			return (1);					\
 		else if (cpusupport_ ## arch ## _ ## feature ## _init)	\
@@ -21,8 +22,9 @@
 		    cpusupport_ ## arch ##_ ## feature ## _detect();	\
 		cpusupport_ ## arch ## _ ## feature ## _init = 1;	\
 		return (cpusupport_ ## arch ## _ ## feature ## _present); \
-	}
+	}								\
+	struct cpusupport_ ## arch ## _ ## feature ## _dummy
 
-CPUSUPPORT_FEATURE(x86, aesni)
+CPUSUPPORT_FEATURE(x86, aesni);
 
 #endif /* !_CPUSUPPORT_H_ */
