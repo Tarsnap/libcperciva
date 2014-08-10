cc30311cdbd982a4ca9b35330ec9804e7badb26d
cperciva <cperciva@94510cbd-7082-6b3d-ae8c-869d587649c8>
2014-08-10 17:51:14 +0000

    Add include guard.

--- trunk/crypto/crypto_aes.h
+++ trunk/crypto/crypto_aes.h
@@ -1,5 +1,10 @@
+#ifndef _CRYPTO_AES_H_
+#define _CRYPTO_AES_H_
+
 #include <stdint.h>
 
 #include <openssl/aes.h>
 
 void aes_encrypt_block(const uint8_t *, uint8_t *, const AES_KEY *);
+
+#endif /* !_CRYPTO_AES_H_ */
