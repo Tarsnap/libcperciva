84cf2c2c9244154476651671b34b8d3e7eed1079
Colin Percival <cperciva@tarsnap.com>
2017-03-14 03:13:10 -0700

    Add minimal testing of two-argument PARSENUM

--- tests/parsenum/main.c
+++ tests/parsenum/main.c
@@ -41,6 +41,13 @@ parsenum_equal(double x, double y)
 	    target "... ", str, #var, #min, #max);		\
 	parsenum_ret = PARSENUM(&parsenum_x, str, min, max);
 
+#define TEST2_DO(str, var, target)				\
+	var parsenum_x;						\
+	int parsenum_ret;					\
+	fprintf(stderr,						\
+	    "Parsing \"%s\" as %s yields " target "... ", str, #var);	\
+	parsenum_ret = PARSENUM(&parsenum_x, str);
+
 #define CHECK_SUCCESS(target)					\
 	if (parsenum_ret == 0) {				\
 		if (parsenum_equal((double)parsenum_x, (double)target)) { \
@@ -77,6 +84,16 @@ parsenum_equal(double x, double y)
 	CHECK_FAILURE(target);					\
 } while (0)
 
+#define TEST2_SUCCESS(str, var, target) do {			\
+	TEST2_DO(str, var, #target);				\
+	CHECK_SUCCESS(target);					\
+} while (0)
+
+#define TEST2_FAILURE(str, var, target) do {			\
+	TEST2_DO(str, var, #target);				\
+	CHECK_FAILURE(target);					\
+} while (0)
+
 int
 main(int argc, char * argv[])
 {
@@ -88,6 +105,10 @@ main(int argc, char * argv[])
 	TEST4_SUCCESS("123.456", double, 0, 1000, 123.456);
 	TEST4_SUCCESS("1234", size_t, -123, 4000, 1234);
 	TEST4_FAILURE("12345", int, -10, 100, ERANGE);
+
+	TEST2_SUCCESS("234.567", double, 234.567);
+	TEST2_SUCCESS("2345", size_t, 2345);
+	TEST2_FAILURE("abcd", size_t, EINVAL);
 
 	/* Success! */
 	exit(0);
