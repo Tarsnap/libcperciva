0c73bbdbccf2d3e59fdc437abc1f2728b2b92cd8
Colin Percival <cperciva@tarsnap.com>
2017-03-16 02:51:24 -0700

    Avoid signed-unsigned comparisons
    When making sure that the range [min, max] is non-empty and
    intersects with the range [0, *(x) = -1], check signs first
    and then compare unsigned values.

--- util/parsenum.h
+++ util/parsenum.h
@@ -63,7 +63,9 @@ _Pragma("clang diagnostic pop")
 			((*(x)) = parsenum_float((s), (min), (max))) :	\
 		(((*(x)) = -1) <= 0) ?					\
 			((*(x)) = parsenum_signed((s), (min), (max))) :	\
-		(((min) > (max)) || ((max) < 0) || ((min) > *(x))) ?	\
+		(((max) < 0) || (((min) > 0) &&				\
+		    (((uintmax_t)(min) > (uintmax_t)(*(x))) ||		\
+		     ((uintmax_t)(min) > (uintmax_t)(max))))) ?		\
 			errno = ERANGE :				\
 			((*(x)) = parsenum_unsigned((s),		\
 			    (min) <= 0 ? 0 : (min),			\
