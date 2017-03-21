e4576b85c49292d9aa707b9eefd2ba4bb804b055
Graham Percival <gperciva@tarsnap.com>
2017-03-20 17:02:13 -0700

    REBASE portability fix for TEST2()

--- tests/parsenum/main.c
+++ tests/parsenum/main.c
@@ -34,8 +34,13 @@ main(int argc, char * argv[])
 
 #define TEST2(x, y) do {				\
 	fprintf(stderr, "PARSENUM(\"%s\")\n", y);	\
-	if (PARSENUM(x, y))				\
-		warnp("PARSENUM");			\
+	if (PARSENUM(x, y)) {				\
+		if (errno == ERANGE) {			\
+			warn0("PARSENUM: ERANGE");	\
+		} else {				\
+			warnp("PARSENUM");		\
+		}					\
+	}						\
 	fprintf(stderr, "%f %zu %d\n", d, s, i);	\
 } while (0)
 
