7e2e8f6df7363377e75812a154dc4522d9d2a354
Graham Percival <gperciva@tarsnap.com>
2017-03-23 19:33:25 -0700

    REBASE second draft of improved testing macros

--- tests/parsenum/main.c
+++ tests/parsenum/main.c
@@ -4,6 +4,46 @@
 #include "parsenum.h"
 #include "warnp.h"
 
+#define PARSENUM_CMP(x, y)						\
+	(fabs((double)x - (double)y) < 1 &&				\
+	    fabs((double)x - (double)y) <= fabs((double)x + (double)y) * 1e-6)
+
+#define TEST4_DO(str, var, min, max, target)			\
+	var parsenum_x;						\
+	int parsenum_ret;					\
+	fprintf(stderr,						\
+	    "Parsing \"%s\" as %s between %s and %s (incl.) yields "	\
+	    target "... ", str, #var, #min, #max);		\
+	parsenum_ret = PARSENUM(&parsenum_x, str, min, max);
+
+#define CHECK_SUCCESS(target)					\
+	if (parsenum_ret == 0) {				\
+		if (PARSENUM_CMP(parsenum_x, target)) {		\
+			fprintf(stderr, "PASSED!\n");		\
+		} else {					\
+			fprintf(stderr, "FAILED!\n");		\
+			goto err0;				\
+		}						\
+	}
+
+#define CHECK_FAILURE(target)					\
+	if (parsenum_ret != 0) {				\
+		if (errno == target)				\
+			fprintf(stderr, "PASSED!\n");		\
+		else						\
+			fprintf(stderr, "FAILED!\n");		\
+	}
+
+#define TEST4_SUCCESS(str, var, min, max, target) do {		\
+	TEST4_DO(str, var, min, max, #target);			\
+	CHECK_SUCCESS(target);					\
+} while (0)
+
+#define TEST4_FAILURE(str, var, min, max, target) do {		\
+	TEST4_DO(str, var, min, max, #target);			\
+	CHECK_FAILURE(target);					\
+} while (0)
+
 int
 main(int argc, char * argv[])
 {
@@ -16,16 +56,9 @@ main(int argc, char * argv[])
 
 	WARNP_INIT;
 
-#define TEST(x, y, z, w) do {						\
-	fprintf(stderr, "PARSENUM(\"%s\", %s, %s)\n", y, #z, #w);	\
-	if (PARSENUM(x, y, z, w))					\
-		warnp("PARSENUM");					\
-	fprintf(stderr, "%f %zu %d\n", d, s, i);			\
-} while (0)
-
-	TEST(&d, "123.456", 0, 1000);
-	TEST(&s, "1234", -123, 4000);
-	TEST(&i, "12345", -10, 100);
+	TEST4_SUCCESS("123.456", double, 0, 1000, 123.456);
+	TEST4_SUCCESS("1234", size_t, -123, 4000, 1234);
+	TEST4_FAILURE("12345", int, -10, 100, ERANGE);
 
 #define TEST2(x, y) do {				\
 	fprintf(stderr, "PARSENUM(\"%s\")\n", y);	\
