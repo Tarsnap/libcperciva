2f5ceb66ee9e1dd8b6c2d2b74dbfa00eae538b38
Graham Percival <gperciva@tarsnap.com>
2017-08-31 10:34:24 -0700

    test_getopt: comment about ignoring the exit code
    It took me a while to realize this was deliberate (after I tried to get the
    test to fail by returning 1), so future me would appreciate a comment to that
    effect.

--- tests/getopt/test_getopt.sh
+++ tests/getopt/test_getopt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# The `|| true` ignores the return value of `test_getopt_args.sh`.  This is
+# deliberate, since we want to rely on comparing the output with good output
+# to determine success or failure.
 printf "Testing command-line options parsing... "
 /bin/sh -e test_getopt_args.sh 2>test_getopt.log || true
 if cmp -s test_getopt.log test_getopt.good; then
