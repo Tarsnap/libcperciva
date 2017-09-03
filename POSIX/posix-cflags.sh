915a21dfca1f05e06e8dd18e36ee24ac6b396ea2
Graham Percival <gperciva@tarsnap.com>
2017-09-03 16:06:26 -0700

    POSIX: add POSIXFAIL_CLOCK_GETTIME for clock_gettime()
    OSX 10.11 and below did not include clock_gettime(), despite it being a
    mandatory part of POSIX.

--- POSIX/posix-cflags.sh
+++ POSIX/posix-cflags.sh
@@ -21,6 +21,24 @@ if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_realtime.c 2>/dev/null; then
 	printf %s "-DPOSIXFAIL_CLOCK_REALTIME"
 	echo "WARNING: POSIX violation: <time.h> not defining CLOCK_REALTIME" 1>&2
 fi
+rm -f posix-clock_gettime
+if ! ${CC} -D_POSIX_C_SOURCE=200809L -o posix-clock_gettime posix-clock_gettime.c 2>/dev/null; then
+	[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
+	printf %s "-DPOSIXFAIL_CLOCK_GETTIME"
+	echo "WARNING: POSIX violation: <time.h> not declaring clock_gettime()" 1>&2
+else
+	# We need to run the binary, because OSX 10.11 (and below) uses dynamic
+	# linking to a library which doesn't include clock_gettime().
+	# Annoyingly, there's two levels of error output: one from the binary
+	# itself, and one from the signal it sends to the calling process.
+	# The "$( ./x 2>y ) 2>y" captures both types of error message.
+	if ! $( ./posix-clock_gettime 2>/dev/null ) 2>/dev/null ; then
+		[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
+		printf %s "-DPOSIXFAIL_CLOCK_GETTIME"
+		echo "WARNING: POSIX violation: clock_gettime() is not linkable" 1>&2
+	fi
+fi
+rm -f posix-clock_gettime
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-restrict.c 2>/dev/null; then
 	echo "WARNING: POSIX violation: ${CC} does not accept 'restrict' keyword" 1>&2
 	if ${CC} -D_POSIX_C_SOURCE=200809L -std=c99 posix-restrict.c 2>/dev/null; then
