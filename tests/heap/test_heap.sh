fcfeaa8b28003e25c2d401b33311b849fd1c8975
Graham Percival <gperciva@tarsnap.com>
2017-09-10 17:38:17 -0700

    test_heap: move test logic from Makefile to .sh

---
+++ tests/heap/test_heap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+printf "Heapsorting main.c..."
+./test_heap < main.c > main.c.sorted
+if LC_ALL=C sort < main.c | cmp - main.c.sorted; then
+	echo " PASSED!"
+	rm main.c.sorted
+else
+	echo " FAILED!"
+fi
