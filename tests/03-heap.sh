3088e40386f684b502fa4f077737b58deb63e696
Graham Percival <gperciva@tarsnap.com>
2017-10-31 10:25:32 -0700

    tests: support failure in an indirect test
    This allows the main test framework to handle a FAIL, instead of immediately
    quitting.

--- tests/03-heap.sh
+++ tests/03-heap.sh
@@ -7,7 +7,7 @@ test_output="${s_basename}-stdout.txt"
 ### Actual command
 scenario_cmd() {
 	setup_check_variables
-	cd ${scriptdir}/heap/ && \
-	    . ./test_heap.sh 1> ${test_output}
+	cd ${scriptdir}/heap/ && c_valgrind_cmd=${c_valgrind_cmd} \
+	    ./test_heap.sh 1> ${test_output}
 	echo $? > ${c_exitfile}
 }
