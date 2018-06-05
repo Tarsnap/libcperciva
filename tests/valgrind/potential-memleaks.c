5f04418768da21a9cf21853f23c0cfbeb1400350
Graham Percival <gperciva@tarsnap.com>
2018-06-05 16:07:57 -0700

    tests/valgrind: list tests, and run one test at once

--- tests/valgrind/potential-memleaks.c
+++ tests/valgrind/potential-memleaks.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Problem with FreeBSD 11.0 merely linking with -lrt. */
 static void
@@ -29,14 +30,33 @@ pl_freebsd_getdelim(void)
 		printf("error in getline()\n");
 }
 
-int
-main(void)
-{
+#define MEMLEAKTEST(x) { #x, x }
+static const struct memleaktest {
+	const char * name;
+	void (* func)(void);
+} tests[] = {
+	MEMLEAKTEST(pl_freebsd_link_lrt),
+	MEMLEAKTEST(pl_freebsd_printf),
+	MEMLEAKTEST(pl_freebsd_getdelim)
+};
+static const int num_tests = sizeof(tests) / sizeof(tests[0]);
 
-	/* Test potential memory leaks. */
-	pl_freebsd_link_lrt();
-	pl_freebsd_printf();
-	pl_freebsd_getdelim();
+int
+main(int argc, char * argv[])
+{
+	int i;
+
+	if (argc == 2) {
+		/* Run the relevant function. */
+		for (i = 0; i < num_tests; i++) {
+			if ((strcmp(argv[1], tests[i].name)) == 0)
+				tests[i].func();
+		}
+	} else {
+		/* Print test names. */
+		for (i = 0; i < num_tests; i++)
+			printf("%s\n", tests[i].name);
+	}
 
 	/* Success! */
 	exit(0);
