3ec27b781adcda7dc0275fddc1c425f1c197d2a2
Graham Percival <gperciva@tarsnap.com>
2018-11-24 17:23:35 -0800

    tests/aws: sanity test for AWS code

---
+++ tests/11-aws.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+### Constants
+c_valgrind_min=1
+test_output="${s_basename}-stderr.txt"
+
+### Actual command
+scenario_cmd() {
+	setup_check_variables
+	cd ${scriptdir}/aws/ && c_valgrind_cmd=${c_valgrind_cmd} \
+	    ./test_aws 2> ${test_output}
+	echo "$?" > ${c_exitfile}
+
+	setup_check_variables
+	cmp ${scriptdir}/aws/test_aws.good ${test_output}
+	echo $? > ${c_exitfile}
+}
