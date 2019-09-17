3ebb957195d4790d5055c34fe8d662a2e78bbeb3
Graham Percival <gperciva@tarsnap.com>
2019-09-16 23:20:58 -0700

    tests/parsenum: direct all stderr to the logfile
    In some platforms [1] if there's an abort trap, the *shell* reports that error.
    So if we want to suppress that message, we need to wrap the offending program
    in a subshell.
    
    [1] Definitely happens on FreeBSD, and I remember having to use this trick for
    something else on MacOS X.

--- tests/parsenum/test_parsenum_improper_usage.sh
+++ tests/parsenum/test_parsenum_improper_usage.sh
@@ -5,16 +5,16 @@ rm -f $log
 
 # Check assertion failures, separating lines so that we automatically check
 # that test_parsenum is giving a non-zero exit code.
-! ./test_parsenum 1 2> $log
+( ! ./test_parsenum 1 ) 2>$log
 grep -q "PARSENUM applied to signed integer without specified bounds" $log
 
-! ./test_parsenum 2 2> $log
+( ! ./test_parsenum 2 ) 2>$log
 grep -q "PARSENUM_EX applied to signed integer without specified bounds" $log
 
-! ./test_parsenum 3 2> $log
+( ! ./test_parsenum 3 ) 2> $log
 grep -q "PARSENUM_EX applied to float" $log
 
-! ./test_parsenum 4 2> $log
+( ! ./test_parsenum 4 ) 2> $log
 grep -q "PARSENUM_EX applied to float" $log
 
 rm $log
