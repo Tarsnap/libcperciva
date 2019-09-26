fc0a4d69f31bc2b279c6186124e0761a322562cf
Graham Percival <gperciva@tarsnap.com>
2019-09-26 12:12:28 -0700

    tests/parsenum: avoid unintended console output
    Different shells on different platforms deal with abort() differently.
    These changes allow test_parsenum_improper_usage.sh to work on Ubuntu,
    FreeBSD, and MacOS X, without unintended output going to stdout or
    stderr.

--- tests/parsenum/test_parsenum_improper_usage.sh
+++ tests/parsenum/test_parsenum_improper_usage.sh
@@ -1,20 +1,45 @@
-#!/bin/sh -e
+#!/bin/sh
 
+# Temporary output
 log=abort.log
-rm -f $log
 
-# Check assertion failures, separating lines so that we automatically check
-# that test_parsenum is giving a non-zero exit code.
-! ./test_parsenum 1 2> $log
-grep -q "PARSENUM applied to signed integer without specified bounds" $log
+# Check test_parsenum case
+run_test() {
+	num=$1
+	reason=$2
 
-! ./test_parsenum 2 2> $log
-grep -q "PARSENUM_EX applied to signed integer without specified bounds" $log
+	# Run the command with no command-line output.  The binary should
+	# produce an abort(), which sends a SIGABRT to the calling process.
+	# Upon receiving this signal, different shells react in different
+	# ways.  In order to avoid polluting the output of `make test`, we
+	# redirect any such message to /dev/null.  We found that redirecting
+	# from both () and $() was necessary to cover Linux, FreeBSD, and
+	# MacOS X.
+	(
+	    $(./test_parsenum $1 2>${log}) 2>/dev/null
+	) 2>/dev/null
+	rc=$?
 
-! ./test_parsenum 3 2> $log
-grep -q "PARSENUM_EX applied to float" $log
+	# Check the exit code.  Many shells will set the exit code to 134
+	# (being 128 + SIGABRT), but some do not.  POSIX merely specifies that
+	# the value shall be greater than 128.
+	if [ ! "$rc" -gt 128 ]; then
+		printf "Return value not higher than 128\n"
+		exit 1
+	fi
 
-! ./test_parsenum 4 2> $log
-grep -q "PARSENUM_EX applied to float" $log
+	# Check the reason for this abort.
+	if ! grep -q "${reason}" ${log} ; then
+		printf "Correct reason not found in ${log}\n"
+		exit 1
+	fi
+}
 
+# Run tests.  Test 4 adds min/max bounds to the PARSENUM_EX call in test 3.
+run_test 1 "PARSENUM applied to signed integer without specified bounds"
+run_test 2 "PARSENUM_EX applied to signed integer without specified bounds"
+run_test 3 "PARSENUM_EX applied to float with base != 0"
+run_test 4 "PARSENUM_EX applied to float with base != 0"
+
+# Clean up
 rm $log
