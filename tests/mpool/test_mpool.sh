59cd49c82c4ea4d4f05c0c900ba5238ea59ef581
Graham Percival <gperciva@tarsnap.com>
2019-09-27 23:38:02 -0700

    tests/mpool: alternate malloc and mpool runs
    This makes the benchmark slightly more robust against CPU frequency
    scaling and changing workloads.  (I'm primarily thinking of Travis-CI
    for the latter concern.)

--- tests/mpool/test_mpool.sh
+++ tests/mpool/test_mpool.sh
@@ -29,36 +29,53 @@ BINARY_REPS=$( make_count ${REPEATS} )
 run() {
 	SETS=$1
 	REPS=$2
-	MPOOL=$3
-	SUFFIX_THIS=$4
+	SUFFIX=$3
 
 	# "Warm up"; don't record this data
 	for j in ${WARMUP_REPS}; do
-		./test_mpool ${SETS} ${REPS} ${MPOOL} > /dev/null
+		./test_mpool ${SETS} ${REPS} 0 > /dev/null
+		./test_mpool ${SETS} ${REPS} 1 > /dev/null
 	done
 
-	arr=""
+	arr_malloc=""
+	arr_mpool=""
 	# Get raw data
 	for i in ${BINARY_REPS}; do
-		usec=$( ./test_mpool ${SETS} ${REPS} ${MPOOL} )
-		arr="${arr} ${usec}"
+		usec=$( ./test_mpool ${SETS} ${REPS} 0 )
+		arr_malloc="${arr_malloc} ${usec}"
+		usec=$( ./test_mpool ${SETS} ${REPS} 1 )
+		arr_mpool="${arr_mpool} ${usec}"
 	done
 
 	# Save data to log (if applicable)
 	if [ -n "${LOGFILE}" ]; then
-		filename="${LOGFILE}-${SUFFIX_THIS}.txt"
-		rm -f ${filename}
 		num=$(( SETS * REPS ))
-		for a in ${arr}; do
+
+		# Write malloc times
+		filename="${LOGFILE}-${SUFFIX}-malloc.txt"
+		rm -f ${filename}
+		for a in ${arr_malloc}; do
+			amortized=$( echo "scale=8;${a} / ${num};" | bc)
+			echo ${amortized} >> ${filename}
+		done
+
+		# Write mpool times
+		filename="${LOGFILE}-${SUFFIX}-mpool.txt"
+		rm -f ${filename}
+		for a in ${arr_mpool}; do
 			amortized=$( echo "scale=8;${a} / ${num};" | bc)
 			echo ${amortized} >> ${filename}
 		done
 	fi
 
 	## Sort array, find minimum
-	sorted_arr=$( echo ${arr} | tr " " "\n" | sort -n | tr "\n" " " )
+	sorted_arr=$( echo ${arr_malloc} | tr " " "\n" | sort -n | tr "\n" " " )
 	lowest=$( echo ${sorted_arr} | cut -d ' ' -f 1 )
-	val=${lowest}
+	val_malloc=${lowest}
+
+	sorted_arr=$( echo ${arr_mpool} | tr " " "\n" | sort -n | tr "\n" " " )
+	lowest=$( echo ${sorted_arr} | cut -d ' ' -f 1 )
+	val_mpool=${lowest}
 
 	return 0
 }
@@ -69,10 +86,9 @@ cmp_methods () {
 	PERCENT_CUTOFF=$3
 	SUFFIX=$4
 
-	run $SETS $REPS 0 "${SUFFIX}-malloc"
-	malloc=${val}
-	run $SETS $REPS 1 "${SUFFIX}-mpool"
-	mpool=${val}
+	run $SETS $REPS "${SUFFIX}"
+	malloc=${val_malloc}
+	mpool=${val_mpool}
 
 	# Sanity check for 0-duration result
 	if [ "${malloc}" -eq "0" ] || [ "${mpool}" -eq "0" ]; then
