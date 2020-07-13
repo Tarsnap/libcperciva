8fd640791877f795a3a52f9de3eb17f4decc93e0
Graham Percival <gperciva@tarsnap.com>
2020-07-12 20:08:39 -0700

    tests/sha256: allow external $CFLAGS
    I thought it might be nice to allow be able to apply extra $CFLAGS to
    the whole test, e.g.,
        CFLAGS="-DSPECIAL=1" sh perftests.sh gcc clang
    or
        CFLAGS="-ffast-math" sh perftests.sh gcc clang
    
    (Not that I endorse using -ffast-math for anything security-related!)

--- tests/sha256/perftest.sh
+++ tests/sha256/perftest.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+CFLAGS_ORIG="${CFLAGS:-}"
+
 for COMPILER in "$@"; do
 	for OFLAG in "-O2" "-O3 -march=native"; do
 		export CC="$COMPILER"
-		export CFLAGS="${OFLAG}"
+		if [ -n "${CFLAGS_ORIG}" ]; then
+			export CFLAGS="${CFLAGS_ORIG} ${OFLAG}"
+		else
+			export CFLAGS="${OFLAG}"
+		fi
 		make clean all >/dev/null 2>/dev/null
 		echo "$CC $CFLAGS"
 		seq 3 | while read X; do
