89e7e5eb4f43b7e86227965abea1c5614776a63e
Graham Percival <gperciva@tarsnap.com>
2020-07-14 10:09:41 -0700

    tests/readpass_file: fix equality tests and failures
    - simplify failure checks
      Reported by:	shellcheck
    
    - == isn't a thing in sh!
      (this one wasn't shellcheck)

--- tests/readpass_file/test_readpass_file.sh
+++ tests/readpass_file/test_readpass_file.sh
@@ -4,39 +4,36 @@ set -e
 
 printf "%s\n" "--- should pass:"
 ./test_readpass_file good-noeol.pass
-test `./test_readpass_file good-noeol.pass | wc -l` == "1"
+test `./test_readpass_file good-noeol.pass | wc -l` -eq "1"
 
 ./test_readpass_file good-eol.pass
-test `./test_readpass_file good-eol.pass | wc -l` == "1"
+test `./test_readpass_file good-eol.pass | wc -l` -eq "1"
 
 ./test_readpass_file good-eol-r.pass
-test `./test_readpass_file good-eol-r.pass | wc -l` == "1"
+test `./test_readpass_file good-eol-r.pass | wc -l` -eq "1"
 
 printf "%s\n" "--- should pass (but will contain extra whitespace):"
 ./test_readpass_file good-spaces.pass
-test `./test_readpass_file good-spaces.pass | wc -l` == "1"
+test `./test_readpass_file good-spaces.pass | wc -l` -eq "1"
 
 ./test_readpass_file good-tabs.pass
-test `./test_readpass_file good-tabs.pass | wc -l` == "1"
+test `./test_readpass_file good-tabs.pass | wc -l` -eq "1"
 
 printf "%s\n" "--- should pass: (but will be a blank password)"
 ./test_readpass_file good-empty.pass
-test `./test_readpass_file good-empty.pass | wc -l` == "1"
+test `./test_readpass_file good-empty.pass | wc -l` -eq "1"
 
 ./test_readpass_file good-only-newline.pass
-test `./test_readpass_file good-only-newline.pass | wc -l` == "1"
+test `./test_readpass_file good-only-newline.pass | wc -l` -eq "1"
 
+# These lines should fail, which means that the "&& exit 1" won't be executed.
 printf "%s\n" "--- should fail:"
-! ./test_readpass_file this-file-does-not-exist
-if [ $? != "0" ]; then exit 1; fi
+./test_readpass_file this-file-does-not-exist && exit 1
 
-! ./test_readpass_file bad-initial-newline.pass
-if [ $? != "0" ]; then exit 1; fi
+./test_readpass_file bad-initial-newline.pass && exit 1
 
-! ./test_readpass_file bad-extra-newline.pass
-if [ $? != "0" ]; then exit 1; fi
+./test_readpass_file bad-extra-newline.pass && exit 1
 
-! ./test_readpass_file bad-extra-material.pass
-if [ $? != "0" ]; then exit 1; fi
+./test_readpass_file bad-extra-material.pass  && exit 1
 
 printf "\nok\n"
