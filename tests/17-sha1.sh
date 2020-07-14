2733cdb72ce70690bc3e706658e3cf40f67043e7
Graham Percival <gperciva@tarsnap.com>
2020-07-14 10:09:37 -0700

    tests: more paranoid checks for failing cd
    Same change as 4fb27f8c17b61a3b97c868eeba9c37f2a14e8ff5, which happened
    at the same time as we were adding 16-md5.sh and 17-sha1.sh.
    
    Reported by:	shellcheck

--- tests/17-sha1.sh
+++ tests/17-sha1.sh
@@ -6,7 +6,7 @@ test_output="${s_basename}-stdout.txt"
 
 ### Actual command
 scenario_cmd() {
-	cd ${scriptdir}/sha1
+	cd ${scriptdir}/sha1 || exit
 
 	setup_check_variables "test_sha1"
 	${c_valgrind_cmd}			\
