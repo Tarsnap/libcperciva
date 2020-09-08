5332c43447e5a399ee1d47101cb9f8f6eaa3ae91
Graham Percival <gperciva@tarsnap.com>
2020-09-08 09:48:52 -0700

    tests/crypto_aesctr/*.py: auto-edit main.c
    Not that we'll be changing the nonce in the future or anything like that.
    Rather, this is intended to reassure us that the output of
    generate_test_vectors.py is actually used in main.c -- namely, run the
    python script, observe that `git diff` doesn't show anything changed,
    then compile & run test_aesctr knowing that the test vectors have the
    same values as the python implementation.

--- tests/crypto_aesctr/generate_test_vectors.py
+++ tests/crypto_aesctr/generate_test_vectors.py
@@ -16,7 +16,43 @@ PLAINTEXTS = [
     ]
 
 
-def generate_for_keylen(keylen, nonce):
+def c_replace_var(c_var_name, c_var_value):
+    """ Replace a variable in 'main.c'. """
+    # Read file.
+    with open("main.c") as fp:
+        lines = fp.readlines()
+
+    # Keep lines until we find the variable name.
+    newfile = ""
+    lookfor = "static const struct testcase %s[] = {\n" % (c_var_name)
+    found = False
+    for i, line in enumerate(lines):
+        newfile += line
+        if line == lookfor:
+            found = True
+            break
+    lines = lines[i + 1:]
+    assert(found)
+
+    # Add new lines to the output.
+    newfile += c_var_value
+    found = False
+    for i, line in enumerate(lines):
+        if line == "};\n":
+            found = True
+            break
+    lines = lines[i:]
+    assert(found)
+
+    # Add the remaining lines
+    newfile += "".join(lines)
+
+    # Write file.
+    with open("main.c", "w") as fp:
+        fp.write(newfile)
+
+
+def generate_for_keylen(keylen, nonce, c_var_name):
     """ Print plaintext and ciphertext for the given key length.  """
     # key: 00 01 02 03 04...
     key_arr = bytearray(b'\0' * keylen)
@@ -27,8 +63,7 @@ def generate_for_keylen(keylen, nonce):
     # Format nonce bytes.
     nonce_bytes = struct.pack(">Q", nonce)
 
-    print("--- test cases for %i-bit AES-CTR" % (keylen * 8))
-
+    formatted = []
     for plaintext in PLAINTEXTS:
         # Always initialize to the same key and initial value (for these cases).
         ctr = Crypto.Util.Counter.new(64, initial_value=0, prefix=nonce_bytes)
@@ -39,11 +74,14 @@ def generate_for_keylen(keylen, nonce):
         ciphertext = aesctr.encrypt(plaintext.encode())
 
         # Format and print for C99.
-        print('\t{"%s",\n\t    "%s",\n\t    "%s"},' % (
+        formatted.append('\t{"%s",\n\t    "%s",\n\t    "%s"}' % (
             binascii.hexlify(key).decode("ascii"),
             plaintext,
             binascii.hexlify(ciphertext).decode("ascii")))
 
+    # Replace variable in main.c.
+    c_var_value = ",\n".join(formatted) + "\n"
+    c_replace_var(c_var_name, c_var_value)
 
-generate_for_keylen(16, 0)
-generate_for_keylen(32, 0)
+generate_for_keylen(16, 0, "tests_128")
+generate_for_keylen(32, 0, "tests_256")
