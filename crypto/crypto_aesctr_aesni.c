7cd0ba8a5527bcc30386293bf795c7c7f6d93e61
Graham Percival <gperciva@tarsnap.com>
2020-12-29 23:52:57 -0800

    crypto_aesctr_aesni: add _wholeblock()
    This change is merely to simplify future diffs; it should not have any
    effect on the program by itself.

--- crypto/crypto_aesctr_aesni.c
+++ crypto/crypto_aesctr_aesni.c
@@ -53,6 +53,39 @@ crypto_aesctr_aesni_stream_cipherblock_use(struct crypto_aesctr * stream,
 	*buflen -= nbytes;
 }
 
+static inline void
+crypto_aesctr_aesni_stream_wholeblock(struct crypto_aesctr * stream,
+    const uint8_t ** inbuf, uint8_t ** outbuf, size_t * buflen)
+{
+	size_t i;
+
+	/* Sanity check. */
+	assert(stream->bytectr % 16 == 0);
+
+	/* Prepare counter. */
+	stream->pblk[15]++;
+	if (stream->pblk[15] == 0) {
+		/*
+		 * If incrementing the least significant byte resulted in it
+		 * wrapping, re-encode the complete 64-bit value.
+		 */
+		be64enc(stream->pblk + 8, stream->bytectr / 16);
+	}
+
+	/* Encrypt the cipherblock. */
+	crypto_aes_encrypt_block(stream->pblk, stream->buf, stream->key);
+
+	/* Encrypt the byte(s). */
+	for (i = 0; i < 16; i++)
+		(*outbuf)[i] = (*inbuf)[i] ^ stream->buf[i];
+
+	/* Update the positions. */
+	stream->bytectr += 16;
+	*inbuf += 16;
+	*outbuf += 16;
+	*buflen -= 16;
+}
+
 /**
  * crypto_aesctr_aesni_stream(stream, inbuf, outbuf, buflen):
  * Generate the next ${buflen} bytes of the AES-CTR stream ${stream} and xor
@@ -83,12 +116,9 @@ crypto_aesctr_aesni_stream(struct crypto_aesctr * stream, const uint8_t * inbuf,
 
 	/* Process blocks of 16 bytes; we need a new cipherblock. */
 	while (buflen >= 16) {
-		/* Generate a block of cipherstream. */
-		crypto_aesctr_aesni_stream_cipherblock_generate(stream);
-
-		/* Encrypt the bytes and update the positions. */
-		crypto_aesctr_aesni_stream_cipherblock_use(stream, &inbuf,
-		    &outbuf, &buflen, 16, 0);
+		/* Generate a cipherblock & use it completely. */
+		crypto_aesctr_aesni_stream_wholeblock(stream, &inbuf,
+		    &outbuf, &buflen);
 	}
 
 	/* Process any final bytes; we need a new cipherblock. */
