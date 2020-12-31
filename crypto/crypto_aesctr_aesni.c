dd164fd0ccce63ba9a9e73b7a2dc44103a1db6f8
Graham Percival <gperciva@tarsnap.com>
2020-12-30 20:52:57 -0800

    REBASE _aesni.c: use generic functions from _internal.h

--- crypto/crypto_aesctr_aesni.c
+++ crypto/crypto_aesctr_aesni.c
@@ -13,49 +13,6 @@
 #include "crypto_aesctr_aesni.h"
 #include "crypto_aesctr_internal.h"
 
-/* Generate a block of cipherstream. */
-static inline void
-crypto_aesctr_aesni_stream_cipherblock_generate(struct crypto_aesctr * stream)
-{
-
-	/* Sanity check. */
-	assert(stream->bytectr % 16 == 0);
-
-	/* Prepare counter. */
-	stream->pblk[15]++;
-	if (stream->pblk[15] == 0) {
-		/*
-		 * If incrementing the least significant byte resulted in it
-		 * wrapping, re-encode the complete 64-bit value.
-		 */
-		be64enc(stream->pblk + 8, stream->bytectr / 16);
-	}
-
-	/* Encrypt the cipherblock. */
-	crypto_aes_encrypt_block(stream->pblk, stream->buf, stream->key);
-}
-
-/* Encrypt ${nbytes} bytes, then update ${inbuf}, ${outbuf}, and ${buflen}. */
-static inline void
-crypto_aesctr_aesni_stream_cipherblock_use(struct crypto_aesctr * stream,
-    const uint8_t ** inbuf, uint8_t ** outbuf, size_t * buflen, size_t nbytes,
-    size_t bytemod)
-{
-	size_t i;
-
-	/* Encrypt the byte(s). */
-	for (i = 0; i < nbytes; i++)
-		(*outbuf)[i] = (*inbuf)[i] ^ stream->buf[bytemod + i];
-
-	/* Move to the next byte(s) of cipherstream. */
-	stream->bytectr += nbytes;
-
-	/* Update the positions. */
-	*inbuf += nbytes;
-	*outbuf += nbytes;
-	*buflen -= nbytes;
-}
-
 static inline void
 crypto_aesctr_aesni_stream_wholeblock(struct crypto_aesctr * stream,
     const uint8_t ** inbuf, uint8_t ** outbuf, size_t * buflen)
@@ -110,13 +67,13 @@ crypto_aesctr_aesni_stream(struct crypto_aesctr * stream, const uint8_t * inbuf,
 		/* Do we have enough to complete the request? */
 		if (bytemod + buflen <= 16) {
 			/* Process only buflen bytes, then return. */
-			crypto_aesctr_aesni_stream_cipherblock_use(stream,
+			crypto_aesctr_stream_cipherblock_use(stream,
 			    &inbuf, &outbuf, &buflen, buflen, bytemod);
 			return;
 		}
 
 		/* Encrypt the byte(s) and update the positions. */
-		crypto_aesctr_aesni_stream_cipherblock_use(stream, &inbuf,
+		crypto_aesctr_stream_cipherblock_use(stream, &inbuf,
 		    &outbuf, &buflen, 16 - bytemod, bytemod);
 	}
 
@@ -130,10 +87,10 @@ crypto_aesctr_aesni_stream(struct crypto_aesctr * stream, const uint8_t * inbuf,
 	/* Process any final bytes; we need a new cipherblock. */
 	if (buflen > 0) {
 		/* Generate a block of cipherstream. */
-		crypto_aesctr_aesni_stream_cipherblock_generate(stream);
+		crypto_aesctr_stream_cipherblock_generate(stream);
 
 		/* Encrypt the byte(s) and update the positions. */
-		crypto_aesctr_aesni_stream_cipherblock_use(stream, &inbuf,
+		crypto_aesctr_stream_cipherblock_use(stream, &inbuf,
 		    &outbuf, &buflen, buflen, 0);
 	}
 }
