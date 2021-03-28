d448257929f7ce97d6460c50068c68cc9758c92c
Graham Percival <gperciva@tarsnap.com>
2021-03-27 17:28:40 -0700

    REBASE stop clang from caring that we're messing with pthread functions

--- libpthread_stubs/pthread_stubs.c
+++ libpthread_stubs/pthread_stubs.c
@@ -1,5 +1,11 @@
 #include <pthread.h>
 
+/* Work around a safety check in clang -- we're replacing these functions. */
+#ifdef __clang__
+_Pragma("clang diagnostic push")
+_Pragma("clang diagnostic ignored \"-Wthread-safety-analysis\"")
+#endif
+
 int
 pthread_mutex_lock(pthread_mutex_t *mutex)
 {
@@ -19,3 +25,8 @@ pthread_mutex_unlock(pthread_mutex_t *mutex)
 	/* Success! */
 	return (0);
 }
+
+/* Work around a safety check in clang. */
+#ifdef __clang__
+_Pragma("clang diagnostic pop")
+#endif
